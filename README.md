976453d29949cd7300057457f5fbbfd64f549810
paulotozi <paulotozi1512@gmail.com>
2023-07-27 00:04:02 -0300

    Atualizando projeto

---
+++ README.md
@@ -0,0 +1,16 @@
+# Website project
+## Description
+
+This is a project involving the basics of HTML and Javascript, the objective being to build a webpage from scratch with the line "418 i'm a teapot!" in Comic Sans being the only element in the HTML file.
+
+## script.js
+
+This file has the script in Javascript, which creates the elements in the HTML file.
+
+## index.html
+
+This file contains the HTML code involved in the construction of the webpage, including <font> that formats the text in a specific font and <body> that contains the phrase.
+
+## result
+
+![webpage](teapot/index.html)
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import * as htmlToImage from "html-to-image";
+
 function addElement() {
     // create a new div element
     const newDiv = document.createElement("div");
@@ -11,4 +13,22 @@ function addElement() {
     // add the newly created element and its content into the DOM
     const currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
-  }
+  } 
+
+const elems = ['.long-text']
+ 
+elems.forEach((elem, ind) => {
+    const node = document.querySelector(elem)
+ 
+    htmlToImage.toPng(node)
+        .then(function (dataUrl) {
+            let img = new Image();
+            img.src = dataUrl;
+            document.body.appendChild(img);
+        })
+        .catch(function (error) {
+            console.error('oops, something went wrong!');
+            console.log(error)
+        });
+})
+ 
